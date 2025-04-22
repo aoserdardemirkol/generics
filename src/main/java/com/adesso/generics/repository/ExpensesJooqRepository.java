60dd0a7f6c9f40b34f9aeceb609ed5b0c8ec3887
aoserdardemirkol <aoserdardemirkol@gmail.com>
2025-04-22 17:11:08 +0300

    -adds flyway

--- src/main/java/com/adesso/generics/repository/ExpensesJooqRepository.java
+++ src/main/java/com/adesso/generics/repository/ExpensesJooqRepository.java
@@ -1,7 +1,7 @@
 package com.adesso.generics.repository;
 
 import com.adesso.generics.jooq.JooqRepository;
-import com.adesso.generics.jooq.generated.public_.tables.records.ExpensesRecord;
+import com.adesso.generics.jooq.generated.tables.records.ExpensesRecord;
 import com.adesso.generics.model.Expenses;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 public class ExpensesJooqRepository extends JooqRepository<Expenses, ExpensesRecord, Long> {
 
     public ExpensesJooqRepository(DSLContext dsl) {
-        super(dsl, Expenses.class, com.adesso.generics.jooq.generated.public_.tables.Expenses.EXPENSES, ExpensesRecord.class);
+        super(dsl, Expenses.class, com.adesso.generics.jooq.generated.tables.Expenses.EXPENSES, ExpensesRecord.class);
     }
 }
--- src/main/java/com/adesso/generics/repository/ProductsJooqRepository.java
+++ src/main/java/com/adesso/generics/repository/ProductsJooqRepository.java
@@ -1,7 +1,7 @@
 package com.adesso.generics.repository;
 
 import com.adesso.generics.jooq.JooqRepository;
-import com.adesso.generics.jooq.generated.public_.tables.records.ProductsRecord;
+import com.adesso.generics.jooq.generated.tables.records.ProductsRecord;
 import com.adesso.generics.model.Products;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 public class ProductsJooqRepository extends JooqRepository<Products, ProductsRecord, Long> {
 
     public ProductsJooqRepository(DSLContext dsl) {
-        super(dsl, Products.class, com.adesso.generics.jooq.generated.public_.tables.Products.PRODUCTS, ProductsRecord.class);
+        super(dsl, Products.class, com.adesso.generics.jooq.generated.tables.Products.PRODUCTS, ProductsRecord.class);
     }
 }
